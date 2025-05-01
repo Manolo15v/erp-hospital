8c08bec22cbddb65754197bfaa11f1775e56a863
Manuel <manolo15v@gmail.com>
2025-05-01 09:17:30 -0400

    Correccion de endpoint en historial

--- src/Citas/routes/historial.routes.js
+++ src/Citas/routes/historial.routes.js
@@ -3,7 +3,7 @@ import{getAllCitas, borrarCita} from "../controllers/historial.controller.js";
 
 const router= Router();
 
-router.get("",getAllCitas);
+router.get("/",getAllCitas);
 router.delete("/:id", borrarCita);
 
 export default router;
